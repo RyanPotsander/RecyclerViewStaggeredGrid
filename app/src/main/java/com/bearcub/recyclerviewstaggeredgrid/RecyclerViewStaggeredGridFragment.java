4f12970d21cc142a9bf9cf18fb9ec22c2c9f321f
rppinfl <ryan.potsander@gmail.com>
2015-05-02 19:54:13 -0500

    Initial commit

---
+++ app/src/main/java/com/bearcub/recyclerviewstaggeredgrid/RecyclerViewStaggeredGridFragment.java
@@ -0,0 +1,13 @@
+package com.bearcub.recyclerviewstaggeredgrid;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Home on 5/2/2015.
+ */
+public class RecyclerViewStaggeredGridFragment extends Fragment {
+
+    public RecyclerViewStaggeredGridFragment(){
+
+    }
+}
